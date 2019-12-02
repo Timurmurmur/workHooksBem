454332db034c9251a88b8a5e44d56ecc0bcc5811
Timur Bartenev <bartimurmurmur@gmail.com>
2019-12-02 13:17:04 +0300

    eslint hasky prettier edit

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,64 +1,88 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { applyMiddleware, combineReducers, createStore, Middleware } from "redux";
+import {
+    applyMiddleware,
+    combineReducers,
+    createStore,
+    Middleware
+} from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { combineEpics, createEpicMiddleware, EpicMiddleware } from 'redux-observable';
-import { initializeCurrentLocation, State as RouterState } from 'redux-little-router';
+import {
+    combineEpics,
+    createEpicMiddleware,
+    EpicMiddleware
+} from 'redux-observable';
+import {
+    initializeCurrentLocation,
+    State as RouterState
+} from 'redux-little-router';
 import createBrowserHistory from 'history/createBrowserHistory';
 
 import { Fragment, RouterActions } from 'redux-little-router';
 
-import { enhancer, middleware, routesMiddleware, reducer as router } from './router';
+import {
+    enhancer,
+    middleware,
+    routesMiddleware,
+    reducer as router
+} from './router';
 
-
-import { MainAction } from "../Main/action";
-import { MainContainer } from "../Main/MainContainer";
+import { MainAction } from '../Main/action';
+import { MainContainer } from '../Main/MainContainer';
 import { Reg } from '../Reg';
 import { Auth } from '../Auth';
 import { AppHeader } from '../AppHeader/AppHeader';
 import { Main } from '../Main/Main';
 
-
 export type Action = RouterActions | MainAction;
-export interface EpicDeps {/* nothing */}
-export interface State extends RouterState {/* nothing */}
+export interface EpicDeps {
+    /* nothing */
+}
+export interface State extends RouterState {
+    /* nothing */
+}
 
-const createMiddleware = (epicMiddleware: EpicMiddleware<Action, Action, State, EpicDeps>) =>
-    applyMiddleware(middleware, epicMiddleware, routesMiddleware);
+const createMiddleware = (
+    epicMiddleware: EpicMiddleware<Action, Action, State, EpicDeps>
+) => applyMiddleware(middleware, epicMiddleware, routesMiddleware);
 
 export const App: React.FC = () => {
-  const history = createBrowserHistory();
-  const composeEnhancers = composeWithDevTools({ serialize: true });
-  const epicMiddleware = createEpicMiddleware<Action, Action, State, EpicDeps>({ dependencies: {} });
+    const history = createBrowserHistory();
+    const composeEnhancers = composeWithDevTools({ serialize: true });
+    const epicMiddleware = createEpicMiddleware<
+        Action,
+        Action,
+        State,
+        EpicDeps
+    >({ dependencies: {} });
 
-  const store = createStore<State, Action, {}, {}>(
-      combineReducers({ router }),
-      composeEnhancers(enhancer, createMiddleware(epicMiddleware))
-  );
+    const store = createStore<State, Action, {}, {}>(
+        combineReducers({ router }),
+        composeEnhancers(enhancer, createMiddleware(epicMiddleware))
+    );
 
-  epicMiddleware.run(combineEpics());
+    epicMiddleware.run(combineEpics());
 
-  const initialState = store.getState();
+    const initialState = store.getState();
 
-  if (initialState && initialState.router) {
-    store.dispatch(initializeCurrentLocation(initialState.router));
-  }
-
-  return (
-    <Provider store={store}>
-      <Fragment forRoute="/">
-        <Fragment forRoute="/">
-          <AppHeader>
-            <MainContainer message={'hello'}/>
-          </AppHeader>
-        </Fragment>
-      </Fragment>
-      <Fragment forRoute="/auth/login">
-          <Auth/>
-      </Fragment>
-      <Fragment forRoute="/auth/reg">
-        <Reg/>
-      </Fragment>
-    </Provider>
-  )
+    if (initialState && initialState.router) {
+        store.dispatch(initializeCurrentLocation(initialState.router));
+    }
+    return (
+        <Provider store={store}>
+            <Fragment forRoute="/">
+                <Fragment forRoute="/">
+                    <AppHeader>
+                        <MainContainer message={'hello'} />
+                    </AppHeader>
+                </Fragment>
+            </Fragment>
+            <Fragment forRoute="/auth/login">
+                <Auth />
+            </Fragment>
+            <Fragment forRoute="/auth/reg">
+                <Reg />
+            </Fragment>
+        </Provider>
+    );
 };
